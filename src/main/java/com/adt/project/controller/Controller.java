150e1addee16e4e0cfd91160ee569a9ebef934a4
adi247u <adi247u@gmail.com>
2024-04-11 02:21:51 +0530

    Foreign Key Bug Fixed

--- src/main/java/com/adt/project/controller/Controller.java
+++ src/main/java/com/adt/project/controller/Controller.java
@@ -9,31 +9,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adt.project.model.Education;
+import com.adt.project.model.Experience;
+import com.adt.project.model.Header;
+import com.adt.project.model.Project;
 import com.adt.project.model.Resume;
 import com.adt.project.repository.ResumeRepository;
-
 
 @RestController
 public class Controller {
     @Autowired
     ResumeRepository resumeRepository;
+
     @PostMapping("/test")
     public String editProfile(@RequestBody Resume resume) {
         try {
+            for (Education education : resume.getEducation()) {
+                education.setResume(resume);
+            }
+            for (Experience experience : resume.getExperience()) {
+                experience.setResume(resume);
+            }
+            for (Project project : resume.getProject()) {
+                project.setResume(resume);
+            }
+            for (Header header : resume.getHeader()) {
+                header.setResume(resume);
+            }
             resumeRepository.save(resume);
             return "Success";
         } catch (Exception e) {
             return e.toString();
-        }
-    }
-
-    @GetMapping("/get/")
-    public Resume getUser(@RequestParam(value = "name") String name) {
-        try {
-            Resume profile = (Resume) resumeRepository.findByUserName(name);
-            return profile;
-        } catch (Exception e) {
-            return null;
         }
     }
 }
